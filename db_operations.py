4954b305c7ce3c23e48c65472994ea1f4e306702
Vamshi-Krishna-1996 <vamshi@icpro.in>
2021-05-14 17:08:38 +0530

    Newly created

---
+++ db_operations.py
@@ -0,0 +1,38 @@
+import sqlite3
+
+class DBOperations():
+    def __init__(self):
+        self.conn = sqlite3.connect('test.db')
+
+    def createTable(self,table_name):
+        try :
+            self.conn.execute('''CREATE TABLE IF NOT EXISTS ''' + table_name + 
+                        ''' (
+                            Customer_Name VARCHAR(255) PRIMARY KEY NOT NULL,
+                            Customer_Id VARCHAR(18) NOT NULL,
+                            Open_Date DATE(8) NOT NULL,
+                            Last_Consulted_Date DATE(8),
+                            Vaccination_Id Type CHAR(5),
+                            Dr_Name CHAR(255),
+                            State CHAR(5),
+                            Country CHAR(5),
+                            Post_Code INTEGER(5),
+                            DOB DATE(8),
+                            Is_Active CHAR(1)
+                        );''')
+        except:
+            print ('Some error occured')
+
+    def insertData(self,table_name,data):
+        try:
+            self.conn.execute('''INSERT INTO ''' + table_name + '''(Customer_Name, 
+            Customer_Id, Open_Date, Last_Consulted_Date, Vaccination_Id, 
+            Dr_Name,State, Country,DOB, Is_Active) Values''' + data +''';''')
+            self.conn.commit()
+        except:
+            print("Some error occurred in inserting data")
+
+            
+    def close_connection(self):
+        self.conn.close()
+   
---
+++ main.py
@@ -0,0 +1,38 @@
+import db_operations 
+
+def mainFunc():
+
+    db = db_operations.DBOperations()
+
+    #open and read the file
+    with open ("Raw_data.txt", "r") as myfile:
+        data = myfile.read().splitlines()
+
+    main_data = []
+
+
+    # format the data into a tuple
+    for each_line in data:
+        each_line = each_line[1:]
+        parts = each_line.split('|')
+        
+        #remove the header because we 
+        if parts[0] == "H" :
+            pass
+        #insert the data into an array called main_data
+        else:
+            parts[3] = int(parts[3])
+            parts[4] = int(parts[4])
+            parts[9] = int(parts[9])
+            main_data.append(tuple(parts[1:]))
+
+    #insert the rows one by one to respective tables
+    for each_data in main_data:
+        table_name = "Table_" + each_data[7]
+        db.createTable(table_name) #it ignores in case if table already exists
+        db.insertData(table_name,str(each_data))
+        db.close_connection()
+
+
+if __name__ == '__main__':
+    mainFunc()
